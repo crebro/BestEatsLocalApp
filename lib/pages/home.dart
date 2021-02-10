686bafab70c36d8d97c1722b6de455946b4fc380
crebro <creationduwal@gmail.com>
2021-02-10 08:00:05 +0545

    Added a login button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:BestEatsLocal/components/all_coupons.dart';
 import 'package:BestEatsLocal/components/appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -13,6 +15,33 @@ class HomePage extends StatelessWidget {
         children: [
           ApplicationBar(),
           AllCoupons(),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            child: FlatButton(
+                color: Colors.blue,
+                padding: EdgeInsets.symmetric(vertical: 7),
+                onPressed: () async {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.redeem,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    "Login to Start Saving"
+                        .text
+                        .fontFamily(GoogleFonts.nunito().fontFamily)
+                        .bold
+                        .white
+                        .xl2
+                        .make(),
+                  ],
+                )),
+          )
         ],
       ),
     ));
